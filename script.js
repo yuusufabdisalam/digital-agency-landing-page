14b09d0bf96e84a17a94a2df515d70b6cb4eb07f
yuusufabdisalam <yuusufabdisalaam@gmail.com>
2025-02-14 11:55:53 +0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+let link_nav=document.querySelector(".link_drop")
+let drop_down=document.querySelector(".drop_down")
+
+link_nav.addEventListener("click",function(){
+        drop_down.classList.toggle("hidden")
+    })
+
+
+console.log("write")
